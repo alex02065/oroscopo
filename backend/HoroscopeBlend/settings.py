507051ec5081e64681ba126e6fdc7f0afceef520
alex02065 <pinale2008@icloud.com>
2026-06-11 07:54:53 +0000

    nginex

--- backend/HoroscopeBlend/settings.py
+++ backend/HoroscopeBlend/settings.py
@@ -134,4 +134,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4321",
     "http://127.0.0.1:4321",
+    "http://localhost",
+    "http://127.0.0.1",
 ]
